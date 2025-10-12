61f2597afcb775b9d9a1dfbf19b7ff904b7f8450
jxfzzzt <librarianupgrade@163.com>
2025-10-12 21:06:55 +0800

    upgrade library to latest versions and fix breaking changes.

--- src/main/java/Command.java
+++ src/main/java/Command.java
@@ -1,6 +1,6 @@
-import org.apache.logging.log4j.core.tools.picocli.CommandLine;
+import picocli.CommandLine;
 
-    @CommandLine.Command(name="-rh", description = "Sistema de Rh")
+@CommandLine.Command(name="-rh", description = "Sistema de Rh")
     public class Command implements Runnable {
 
     @picocli.CommandLine.Option(names = {})
